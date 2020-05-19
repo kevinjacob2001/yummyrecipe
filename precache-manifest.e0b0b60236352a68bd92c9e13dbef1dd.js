96a35a4a0d60f9248fb29698157bb40df447cd71
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-05-19 19:19:05 +0530

    Updates

---
+++ precache-manifest.e0b0b60236352a68bd92c9e13dbef1dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a72923f8ea815eb34b15bf5d3c6874b",
+    "url": "/yummyrecipe/index.html"
+  },
+  {
+    "revision": "d86a9f23acee0c03bf64",
+    "url": "/yummyrecipe/static/css/main.00036ff9.chunk.css"
+  },
+  {
+    "revision": "a9cc30b951caebaa52fd",
+    "url": "/yummyrecipe/static/js/2.4e0fcbbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yummyrecipe/static/js/2.4e0fcbbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d86a9f23acee0c03bf64",
+    "url": "/yummyrecipe/static/js/main.a21666d2.chunk.js"
+  },
+  {
+    "revision": "bc7eafd39533cdb60c94",
+    "url": "/yummyrecipe/static/js/runtime-main.ad62aa9e.js"
+  }
+]);
